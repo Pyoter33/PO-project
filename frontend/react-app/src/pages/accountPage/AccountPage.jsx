49a99c5b86672e7f35aba68ddd46db3f16919c03
PanMiko <mikolajczyk.kacper2000@gmail.com>
2022-01-22 14:18:17 +0100

    fixed edit button on account page

--- frontend/react-app/src/pages/accountPage/AccountPage.jsx
+++ frontend/react-app/src/pages/accountPage/AccountPage.jsx
@@ -13,13 +13,15 @@ export const AccountPage = () => {
     const [isFetchingData, setIsFetchingData] = useState(true);
 
     useEffect(() => {
-        setIsFetchingData(true);
-        axios.get('http://localhost:5000/users/1')
-        .then(({data}) => {
-            console.log(data);
-            setUserData(data);
-            setIsFetchingData(false);
-        });
+        if (!isModalShown) {
+            setIsFetchingData(true);
+            axios.get('http://localhost:5000/users/1')
+            .then(({data}) => {
+                setUserData(data);
+                setIsFetchingData(false);
+            });
+        }
+        
     }, [isModalShown]);
 
     const transformDate = (dateStr) => {
@@ -37,88 +39,92 @@ export const AccountPage = () => {
 
     const handleOkModal = ({name, surname, login, password}) => {
         axios.patch('http://localhost:5000/users/1', {
-            name: name,
-            surname: surname,
-            login: login,
-            password: password,
+            name,
+            surname,
+            login,
+            password,
         })
-        .then(({data}) => {
-            console.log(data);
-            setUserData(data);
+        .then(() => {
+            handleCloseModal();
         });
-
-        handleCloseModal();
     }
 
-    return (
-        <>
-            <div className={styles.root}>
-                <Tooltip title="Edytuj profil" className={styles.tooltipWithButton}>
-                    <Button 
-                        type="primary" 
-                        icon={<EditOutlined />} 
-                        className={styles.iconButton} 
-                        onClick={() => handleShowModal()}
-                    >
-                        Edytuj
-                    </Button>
-                </Tooltip>
-
-                <img src={avatar} alt='no_image' width={150}></img>
-                <h3 className='pt-3'>{userData.imie} {userData.nazwisko}</h3>
-                <div className={styles.divider} />
-
-                <div className={`${styles.x} mt-3 d-flex justify-content-around text-center`}>
-                    <div className={styles.statCell}>
-                        <h3>10</h3>
-                        <h6>Liczba wycieczek</h6>
+    if (!isFetchingData && userData) {
+        return (
+            <>
+                <div className={styles.root}>
+                    <Tooltip title="Edytuj profil" className={styles.tooltipWithButton}>
+                        <Button 
+                            type="primary" 
+                            icon={<EditOutlined />} 
+                            className={styles.iconButton} 
+                            onClick={() => handleShowModal()}
+                        >
+                            Edytuj
+                        </Button>
+                    </Tooltip>
+    
+                    <img src={avatar} alt='no_image' width={150}></img>
+                    <h3 className='pt-3'>{userData.user.imie} {userData.user.nazwisko}</h3>
+                    <div className={styles.divider} />
+    
+                    <div className={`${styles.x} mt-3 d-flex justify-content-around text-center`}>
+                        <div className={styles.statCell}>
+                            <h3>{userData.totalTrips}</h3>
+                            <h6>Liczba wycieczek</h6>
+                        </div>
+    
+                        <div className={styles.dividerVertical} />
+    
+                        <div className={styles.statCell}>
+                            <h3>{userData.totalBadges}</h3>
+                            <h6>Liczba zdobytych odznak</h6>
+                        </div>
+    
+                        <div className={styles.dividerVertical} />
+    
+                        <div className={styles.statCell}>
+                            <h3>{userData.totalPoints}</h3>
+                            <h6>Liczba zdobytych punktów</h6>
+                        </div>
                     </div>
-
-                    <div className={styles.dividerVertical} />
-
-                    <div className={styles.statCell}>
-                        <h3>30</h3>
-                        <h6>Liczba zdobytych odznak</h6>
-                    </div>
-
-                    <div className={styles.dividerVertical} />
-
-                    <div className={styles.statCell}>
-                        <h3>200</h3>
-                        <h6>Liczba zdobytych punktów</h6>
+    
+                    <div className='mt-5'>
+                        <div className='d-flex'>
+                            <p className={styles.label}>Rola: </p>
+                            <p className={styles.value}>{userData.user.rolanazwa}</p>
+                        </div>
+    
+                        <div className='d-flex'>
+                            <p className={styles.label}>Login: </p>
+                            <p className={styles.value}>{userData.user.login}</p>
+                        </div>
+    
+                        <div className='d-flex'>
+                            <p className={styles.label}>Data urodzenia: </p>
+                            <p className={styles.value}>{transformDate(userData.user.dataurodzenia)}</p>
+                        </div>
                     </div>
                 </div>
+    
+                {
+                    !isFetchingData && isModalShown &&
+                    <UpdateAccountModal 
+                        visible={isModalShown} 
+                        handleCancel={handleCloseModal}
+                        handleOk={handleOkModal}
+                        name={userData.user.imie}
+                        surname={userData.user.nazwisko}
+                        login={userData.user.login}
+                        password={userData.user.haslo}
+                    />
+                }
+            </>
+        );
+    }
 
-                <div className='mt-5'>
-                    <div className='d-flex'>
-                        <p className={styles.label}>Rola: </p>
-                        <p className={styles.value}>{userData.rolauzytkownikaid}</p>
-                    </div>
-
-                    <div className='d-flex'>
-                        <p className={styles.label}>Login: </p>
-                        <p className={styles.value}>{userData.login}</p>
-                    </div>
-
-                    <div className='d-flex'>
-                        <p className={styles.label}>Data urodzenia: </p>
-                        <p className={styles.value}>{transformDate(userData.dataurodzenia)}</p>
-                    </div>
-                </div>
-            </div>
-
-            {
-                !isFetchingData && isModalShown &&
-                <UpdateAccountModal 
-                    visible={isModalShown} 
-                    handleCancel={handleCloseModal}
-                    handleOk={handleOkModal}
-                    name={userData.imie}
-                    surname={userData.nazwisko}
-                    login={userData.login}
-                    password={userData.haslo}
-                />
-            }
-        </>
-    );
+    else {
+        return <h1>Loading...</h1>
+    }
+    
 };
