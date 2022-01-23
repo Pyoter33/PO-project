7644ada8781c822ca2c2546c462a8313582b99b5
PanMiko <mikolajczyk.kacper2000@gmail.com>
2022-01-23 10:52:06 +0100

    fixed side menu highlights on reload page

--- frontend/react-app/src/components/sideBar/SideBar.jsx
+++ frontend/react-app/src/components/sideBar/SideBar.jsx
@@ -1,50 +1,52 @@
 import React from 'react';
 import { Layout, Menu } from 'antd';
 import { HomeOutlined, ContainerOutlined, UserOutlined, InfoCircleOutlined } from '@ant-design/icons';
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import styles from "./SideBar.module.scss";
 
 const { SubMenu } = Menu;
 const { Sider } = Layout;
 
 export const SideBar = () => {
-  return (
-    <Sider width={200} >
-        <Menu
-            mode="inline"
-            defaultSelectedKeys={['1']}
-            className={styles.sidebarMenu}
-        >
+    const location = useLocation();
 
-            <Menu.Item key="1" icon={<HomeOutlined />}>
-                <Link to='/'>
-                    Dashboard
-                </Link>
-            </Menu.Item>
-
-
-            <SubMenu 
-                key="sub3" 
-                icon={<ContainerOutlined />} 
-                title="Wnioski"
+    return (
+        <Sider width={200} >
+            <Menu
+                mode="inline"
+                defaultSelectedKeys={[location.pathname]}
+                className={styles.sidebarMenu}
             >
-                <Menu.Item key="9">
-                    <Link to='requests/update-section'>
-                        Aktualizacja odcinka
+
+                <Menu.Item key="/" icon={<HomeOutlined />}>
+                    <Link to='/'>
+                        Dashboard
                     </Link>
                 </Menu.Item>
 
-                <Menu.Item key="10">...</Menu.Item>
-            </SubMenu>
 
-            <Menu.Item key="2" icon={<UserOutlined />} >
-                <Link to='account'>
-                    Profil
-                </Link>
-            </Menu.Item>
+                <SubMenu 
+                    key="sub_menu" 
+                    icon={<ContainerOutlined />} 
+                    title="Wnioski"
+                >
+                    <Menu.Item key="/requests/update-section">
+                        <Link to='/requests/update-section'>
+                            Aktualizacja odcinka
+                        </Link>
+                    </Menu.Item>
 
-            <Menu.Item key="3" icon={<InfoCircleOutlined />}>...</Menu.Item>
-        </Menu>
-    </Sider>  
-  );
+                    <Menu.Item key="3">...</Menu.Item>
+                </SubMenu>
+
+                <Menu.Item key="/account" icon={<UserOutlined />} >
+                    <Link to='account'>
+                        Profil
+                    </Link>
+                </Menu.Item>
+
+                <Menu.Item key="5" icon={<InfoCircleOutlined />}>...</Menu.Item>
+            </Menu>
+        </Sider>  
+    );
 };
